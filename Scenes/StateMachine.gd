efec3c44e9582e7fb1c49ea3c6371c77cc0cb1a4
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-21 15:24:08 +0000

    add changing state functionality add set to default state

--- Scenes/StateMachine.gd
+++ Scenes/StateMachine.gd
@@ -10,3 +10,20 @@ func _ready() -> void:
 		if child is State:
 			states[child.name] = child
 			child.initialize()
+	
+	if default_state != null:
+		change_state(default_state.name)
+
+func change_state (state_name):
+	var new_state = states.get(state_name)
+	if new_state == null:
+		return
+	if new_state == current_state:
+		return
+	if current_state != null:
+		current_state.exit()
+	
+	current_state = new_state
+	new_state.enter()
+	
+	print("change state : " + state_name)
