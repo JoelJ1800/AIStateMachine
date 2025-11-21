ec1e8905bcdd9f9d1d55b6849ad862a6710c2a72
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-21 19:19:30 +0000

    add limit stop

--- Scenes/ChaseState.gd
+++ Scenes/ChaseState.gd
@@ -9,10 +9,12 @@ var last_path_update_time:float
 func enter():
 	super.enter()
 	controller.is_running = true
+	controller.look_at_player = true
 
 func exit():
 	super.exit()
 	controller.is_running = false
+	controller.look_at_player = false
 
 func update(delta):
 	var current_time = Time.get_unix_time_from_system()
@@ -20,4 +22,6 @@ func update(delta):
 	if current_time - last_path_update_time > path_update_rate:
 		last_path_update_time = current_time
 		controller.move_to_position(controller.player.position, false)
-		
+	
+	if controller.player_distance < stop_range:
+		controller.is_stopped = true
