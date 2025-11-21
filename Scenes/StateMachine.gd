7b72749f761016c3cd64ce02be4dc4532da55caa
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-21 15:20:59 +0000

    auto creaete dict references

--- Scenes/StateMachine.gd
+++ Scenes/StateMachine.gd
@@ -1,2 +1,12 @@
 class_name StateMachine
 extends Node
+
+@export var default_state:State
+var current_state:State
+var states:Dictionary = {}
+
+func _ready() -> void:
+	for child in get_children():
+		if child is State:
+			states[child.name] = child
+			child.initialize()
